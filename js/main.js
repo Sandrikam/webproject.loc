965cf59e105a1fa94d1d84ed9b12aabfa4096871
Sandro Maisuradze <sandromaisuradze32@gmail.com>
2018-12-15 00:23:52 +0400

    Tried to make link list feature but didn't

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-const data = JSON.parse(localStorage.getItem('items',links));
-    console.log(data)//mere damchirdeba
+const data1 = JSON.parse(localStorage.getItem('items'));
+  
+  $(document).ready(function() {
+    $('#TableURLs thead').append(`
+    <tr>
+      <th scope="col" width="5%">#</th>
+      <th scope="col" width="30%" class="url">URL</th>
+      <th scope="col" width="15% class="tag">Tag</th>
+      <th scope="col" width="50%" class="Descr">Description</th>
+    </tr>
+    `);
 
-
-    
-   $('#btn1').on('click', function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-        $('#tableURLs').append(`
-        
-        <thead>
-        <tr>
-          <th scope="col" width="5%">#</th>
-          <th scope="col" width="30%">URL</th>
-          <th scope="col" width="15%">Tag</th>
-          <th scope="col" width="50%">Description</th>
-        </tr>
-      </thead>
-        `);
-   });
+    console.log($('#TableURLs .url').text(data1[0]['url']));
+    Console.log($('#TableURLs .tag').text(data1[0]['tag']));
+    console.log($('#TableURLs .Descr').text(data1[0]['Descr']));
+   console.log(data1);
+  
+  });
